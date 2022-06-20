8f3189fad1bdba8ed9daa7cb6a7738bcd7eb7c76
Eric Shumway <ericshumway2@mail.weber.edu>
2022-06-19 21:22:17 -0600

    changes to client side and server side

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,42 @@
+import {gql} from '@apollo/client'
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: bValues) {
+        saveBook(input: $input) {
+            username
+        }
+    }
+
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation deleteBook($bookId: String!) {
+        deleteBook(bookId: $bookId) {
+            username
+        }
+    }
+`;
